36c2540d38aa29bd0b78d3c8d0d1eccd91937568
jesus cabrera <20183ti014@utez.edu.mx>
2022-11-17 12:53:55 -0600

    add fixes in view camareras

--- js/roomController.js
+++ js/roomController.js
@@ -22,48 +22,50 @@ let getRoomsByUser = (id) =>{
         // Loop to access all rows
         for (let item of data.data) {
             content += ` 
-            <div class="cards-grid habitaciones">
-                <div class="flip-card">
-                    <div class="flip-card-inner"
-                        style="box-shadow: rgba(0, 255, 8, 0.4) 0px 5px, rgba(1, 246, 50, 0.3) 0px 10px, rgba(9, 244, 0, 0.2) 0px 15px, rgba(0, 240, 8, 0.1) 0px 20px, rgba(31, 236, 0, 0.05) 0px 25px;">
-                        <div class="flip-card-front">
-                        <strong class="text-center" id="desabilitado"> ${item.number} </strong>
-                        </div>
-                        <div class="flip-card-back">
-                        <div class="row">
-                            <h3>Acciones</h3>
-                            <div class="col-md-12">
-                            <div class="col-md-12 text-center">
-                                <button type="button" data-mdb-toggle="tooltip" data-mdb-placement="bottom"
-                                title="Limpiar" onClick="limpiar()" class="btn btn-primary btn-floating">
-                                <i class="fa-solid fa-broom"></i>
-                                </button>
+            
+                <div class="cards-grid habitaciones">
+                    <div class="flip-card">
+                        <div class="flip-card-inner"
+                            style="box-shadow: rgba(0, 255, 8, 0.4) 0px 5px, rgba(1, 246, 50, 0.3) 0px 10px, rgba(9, 244, 0, 0.2) 0px 15px, rgba(0, 240, 8, 0.1) 0px 20px, rgba(31, 236, 0, 0.05) 0px 25px;">
+                            <div class="flip-card-front">
+                            <strong class="text-center" id="desabilitado"> ${item.number} </strong>
                             </div>
-                            <br>
+                            <div class="flip-card-back">
+                            <div class="row">
+                                <h3>Acciones</h3>
+                                <div class="col-md-12">
+                                <div class="col-md-12 text-center">
+                                    <button type="button" data-mdb-toggle="tooltip" data-mdb-placement="bottom"
+                                    title="Limpiar" onClick="limpiar()" class="btn btn-primary btn-floating">
+                                    <i class="fa-solid fa-broom"></i>
+                                    </button>
+                                </div>
+                                <br>
+                                </div>
+                                <div class="col-md-12">
+                                <div class="col-md-12 text-center">
+                                    <button type="button" class="btn btn-success btn-floating" data-toggle="modal"
+                                    data-target="#basicExampleModal">
+                                    <i class="fa-solid fa-circle-check"></i>
+                                    </button>
+                                </div>
+                                <br>
+                                </div>
+                                <div class="col-md-12">
+                                <div class="col-md-12 text-center">
+                                    <form action="/pages/history.html">
+                                    <button type="submit" class="btn btn-info btn-floating">
+                                        <i class="fa-solid fa-clock"></i>
+                                    </button>
+                                    </form>
+                                </div>
+                                </div>
                             </div>
-                            <div class="col-md-12">
-                            <div class="col-md-12 text-center">
-                                <button type="button" class="btn btn-success btn-floating" data-toggle="modal"
-                                data-target="#basicExampleModal">
-                                <i class="fa-solid fa-circle-check"></i>
-                                </button>
                             </div>
-                            <br>
-                            </div>
-                            <div class="col-md-12">
-                            <div class="col-md-12 text-center">
-                                <form action="/pages/history.html">
-                                <button type="submit" class="btn btn-info btn-floating">
-                                    <i class="fa-solid fa-clock"></i>
-                                </button>
-                                </form>
-                            </div>
-                            </div>
-                        </div>
                         </div>
                     </div>
                 </div>
-            </div>
+           
             `;
 
         }
