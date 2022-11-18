ff35ae6d25df2afcba0cf43d53a07f0732c0a66a
jesus cabrera <20183ti014@utez.edu.mx>
2022-11-18 10:26:47 -0600

    fix view edificios

--- js/buildingController.js
+++ js/buildingController.js
@@ -1,36 +1,45 @@
 let token = localStorage.getItem("myToken")
 
-window.onload = function() {
+window.onload = function () {
     verifySession()
     console.log('onload');
 }
 
-let getBuildingsByUser = () =>{
+let getBuildingsByUser = () => {
     fetch('http://127.0.0.1:8000/api/building/getAllBelongUser', {
-    method: 'GET',
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: "Bearer " + token,
-    },
-}).then(response => response.json())
-    .then(data => {
-        let content = ``;
-        
-        // Loop to access all rows
-        for (let item of data.data) {
-            content += ` 
-                <tr>
-                    <td>${item.name}</td>
-                    <td><span id="activo" class="badge rounded-pill badge-success">Activo</span></td>
-                    <td><a type="button" href="/pages/camarera.html?id=${item.id}" class="btn btn-info btn-floating">
-                        <i class="fas fa-eye"></i>
-                    </a></td>
-                </tr>
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + token,
+        },
+    }).then(response => response.json())
+        .then(data => {
+            let content = ``;
+
+            // Loop to access all rows
+            for (let item of data.data) {
+                content += ` 
+                <div style="padding:16px 20px" class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                      
+                        <div class="card-flyer">
+                            <div class="text-box">
+                            <div class="image-box">
+                                <img src="../img/hotel.png" alt="" />
+                            </div>
+                            <div class="text-container">
+                                <h6>${item.name}</h6>
+                                <a type="button" href="/pages/camarera.html?id=${item.id}" class="btn btn-info">
+                                    Ver habitaciones </a>
+                            </div>
+                            </div>
+                        </div>
+                      
+                </div>
             `;
 
-        }
-        // Setting innerHTML as tab variable
-        document.getElementById("tableBuilding").innerHTML = content;
-    })
+            }
+            // Setting innerHTML as tab variable
+            document.getElementById("tableBuilding").innerHTML = content;
+        })
 }
