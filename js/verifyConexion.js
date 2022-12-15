36d67d9d8589150feb2146bb59be66e03232c5fd
misaelmtz10 <85245368+misaelmtz10@users.noreply.github.com>
2022-12-15 12:08:01 -0600

    Update verifyConexion.js

--- js/verifyConexion.js
+++ js/verifyConexion.js
@@ -2,29 +2,24 @@ const isOnline = async () =>
 {
     
   if(navigator.onLine){
-      console.log('isOnline');
-      console.log('section ' + section + ' ubication ' + ubication);
       document.querySelector(".theme-loader").setAttribute("style", "display: initial !important; ");
       if (ubication === "room") {
         const paramsUrl = new URLSearchParams(document.location.search)
         const idBul = paramsUrl.get("id")
         switch (section) {
           case "assigned":
-              console.log('entra al caso de assigned');
               setTimeout(() => {
                 getRoomsByUserByStatusAssigned(idBul,1)
               }, 2500);
               document.querySelector(".theme-loader").setAttribute("style", "display: none !important; ");
             break;
           case "blocked":
-              console.log('entra al caso de blocked');
               setTimeout(() => {
                 getRoomsByUserByStatusBlocked(idBul, 4)
               }, 2500);
               document.querySelector(".theme-loader").setAttribute("style", "display: none !important; ");
             break;
           case "cleaned":
-              console.log('entra al caso de cleaned');
               setTimeout(() => {
                 getRoomsByUserByStatusCleaned(idBul,2),
                 getRoomsByUserByStatusBlocked(idBul, 4),
@@ -33,7 +28,6 @@ const isOnline = async () =>
               document.querySelector(".theme-loader").setAttribute("style", "display: none !important; ");
             break;
           default:
-              console.log('entra al default');
               document.querySelector(".theme-loader").setAttribute("style", "display: none !important; ");
             break;
         }
